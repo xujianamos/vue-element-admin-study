f30f88ba0c8cb71dd7e34e99b9275356901b9797
徐建 <xujian@cnksi.com>
2020-12-28 21:50:29 +0800

    ddd

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,8 @@
  * @Author: xujian
  * @Date: 2020-11-17 15:05:53
  * @LastEditors: xujian
- * @LastEditTime: 2020-12-18 16:28:46
- * @Description:
+ * @LastEditTime: 2020-12-28 18:15:34
+ * @Description:数据仓库
  * @FilePath: \vue-element-admin\src\store\index.js
  */
 import Vue from 'vue'
@@ -11,20 +11,21 @@ import Vuex from 'vuex'
 import getters from './getters'
 
 Vue.use(Vuex)
+
 const modulesFiles = require.context('./modules', true, /\.js$/)
 
-// you do not need `import app from './modules/app'`
-// it will auto require all vuex module from modules file
+// 它将自动从模块文件中导入所有的vuex模块
+// console.log(modulesFiles.keys());
+// ["./app.js", "./errorLog.js", "./permission.js", "./settings.js", "./tagsView.js", "./user.js"]
 const modules = modulesFiles.keys().reduce((modules, modulePath) => {
-  console.log(modules)
-  console.log(modulePath)
   // set './app.js' => 'app'
   const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
   const value = modulesFiles(modulePath)
   modules[moduleName] = value.default
   return modules
 }, {})
-console.log(modules)
+
+// console.log(modules)
 const store = new Vuex.Store({
   modules,
   getters
